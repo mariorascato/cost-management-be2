83b8263b70c432b75beaeb616a2dbd85434318c8
Fugax08 <67953267+Fugax08@users.noreply.github.com>
2021-11-07 19:42:42 +0100

    aggiunta gettipologie in tipologiacontrattocontroller

--- src/main/java/cost/management/controller/TipologiaContrattoController.java
+++ src/main/java/cost/management/controller/TipologiaContrattoController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,6 +36,13 @@ public class TipologiaContrattoController {
 		}
 		tipologiaContrattoService.addTipologiaContatto(contratto);
 		return true;
+	}
+
+	
+	@GetMapping("/get_tipologie")
+	public List<TipologiaContratto> findAllTipologiaContrattos()
+	{
+		return tipologiaContrattoService.findAllContratto();
 	}
 
 }
